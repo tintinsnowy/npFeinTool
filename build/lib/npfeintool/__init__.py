397da071449fee60bec4711fc03581e21be1ddbd
tintinsnowy <yangxiaoli1994@gmail.com>
2018-08-06 13:34:27 +0200

    change the import stuff

---
+++ build/lib/npfeintool/__init__.py
@@ -0,0 +1,5 @@
+
+"""
+The __init__.py file is usually empty, but if you remove the __init__.py file, Python will no longer look for submodules inside that directory.
+"""
+# Export public objects
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="npFeintool",
+    version="0.0.1",
+    author="Sherry Yang",
+    author_email="yangxiaoli1994@gmail.com",
+    description="npFeintool is used for the time series data processing",
+    long_description=long_description,
+    #long_description_content_type="text/markdown",
+    url="https://github.com/Senseering/npFeinTool",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix"
+    ),
+)
